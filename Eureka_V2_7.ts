0dfde78ec1ec8e8b6fe18e2d34388c3f1e2e89fe
itibanbosi <51027729+itibanbosi@users.noreply.github.com>
2021-04-28 11:16:28 +0900

    更新 Eureka_V2_7.ts

--- Eureka_V2_7.ts
+++ Eureka_V2_7.ts
@@ -1114,16 +1114,6 @@ namespace eureka_blocks_soro {
 
 namespace newio_blocks {
 
-    //% blockId=newio_first
-    //% block="ｲｰｵのﾈｵﾋﾟｸｾﾙLEDを使う"
-    // group="1_初期設定"
-    //% color="#ff3d03"
-    //% weight=90
-
-    export function newio_first() {
-    let strip = neopixel.create(DigitalPin.P9, 4, NeoPixelMode.RGB)
-  }
-
 
 
     //% color="#4741f1" weight=89 blockId=neopixel_blue block="青信号 点灯|%mode|" group="2_信号機ユニット"
@@ -1141,7 +1131,7 @@ namespace newio_blocks {
     }
   }
 
-    //% color="#ffa800" weight=89 blockId=neopixel_yellow block="黄信号 点灯|%mode|" group="2_信号機ユニット"
+    //% color="#ffa800" weight=86 blockId=neopixel_yellow block="黄信号 点灯|%mode|" group="2_信号機ユニット"
     export function neopixel_yellow_block(mode: onoff) {
     switch (mode) {
       case  onoff.ON :
@@ -1156,7 +1146,7 @@ namespace newio_blocks {
     }
   }
 
-    //% color="#ff4940" weight=89 blockId=neopixel_red block="赤信号 点灯|%mode|" group="2_信号機ユニット"
+    //% color="#ff4940" weight=84 blockId=neopixel_red block="赤信号 点灯|%mode|" group="2_信号機ユニット"
     export function neopixel_red_block(mode: onoff) {
     switch (mode) {
       case  onoff.ON :
@@ -1171,65 +1161,6 @@ namespace newio_blocks {
     }
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-  //% color="#ffa800" weight=87 blockId=neopixel__yellow block="黄信号 点灯|%mode|" group="2_信号機ユニット"
-  export function eureka_tl_yellow(mode: onoff) {
-    switch (pin) {
-      case eureka_tlp.A:
-        if (mode == onoff.ON) {
-          return pins.digitalWritePin(DigitalPin.P13, 1);
-        } else {
-          return pins.digitalWritePin(DigitalPin.P13, 0);
-        }
-      case eureka_tlp.B:
-        if (mode == onoff.ON) {
-          return pins.digitalWritePin(DigitalPin.P15, 1);
-        } else {
-          return pins.digitalWritePin(DigitalPin.P15, 0);
-        }
-    }
-  }
-  //% color="#ff4940" weight=85 blockId=neopixel_red block="赤信号 点灯|%mode|" group="2_信号機ユニット"
-  export function eureka_tl_red(mode: onoff) {
-    switch (pin) {
-      case eureka_tlp.A:
-        if (mode == onoff.ON) {
-          return pins.digitalWritePin(DigitalPin.P0, 1);
-        } else {
-          return pins.digitalWritePin(DigitalPin.P0, 0);
-        }
-      case eureka_tlp.B:
-        if (mode == onoff.ON) {
-          return pins.digitalWritePin(DigitalPin.P1, 1);
-        } else {
-          return pins.digitalWritePin(DigitalPin.P1, 0);
-        }
-    }
-  }
-
-  //% color="#1E90FF" weight=83 block="待ち時間（秒）|%second|" group="2_信号機ユニット"
-  //% second.min=0 second.max=10
-  export function driveForwards(second: number): void {
-    basic.pause(second * 1000);
-  }
-*/
 
 }
 
